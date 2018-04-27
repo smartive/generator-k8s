5237ec4c85adb026fc4d3371430ae10e46238659
Christoph Bühler <christoph@smartive.ch>
2018-04-27 11:40:51 +0200

    feat: add HTTPS question for ingress

--- resourceMetadata/service.js
+++ resourceMetadata/service.js
@@ -4,20 +4,20 @@ module.exports = {
     {
       type: 'input',
       name: 'global-name',
-      message: 'The file name (without extension)',
+      message: 'The file name (without extension)?',
       validate: input => !!!input ? 'Please enter a value' : true,
     },
     {
       type: 'input',
       name: 'global-namespace',
-      message: 'The name of the namespace',
+      message: 'The name of the namespace?',
       default: '${NAMESPACE}',
       validate: input => !!!input ? 'Please enter a value' : true,
     },
     {
       type: 'input',
       name: 'service-name',
-      message: 'The name of the service',
+      message: 'The name of the service?',
       default: '${CI_PROJECT_PATH_SLUG}',
       validate: input => !!!input ? 'Please enter a value' : true,
     },
